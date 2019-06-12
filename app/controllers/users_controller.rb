d8917999743160ffe7ba355485b147cf2cb2e215
Eragon <mdavid.almiron@gmail.com>
2019-06-12 01:03:24 -0300

    CRUD users
    Update funciona correctamente.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,8 +53,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render json: { status: true }, status: :ok, location: @user }
+        # format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render json: {status: 'success'}, status: :created, location: @user }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
