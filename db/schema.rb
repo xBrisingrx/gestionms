69347907020a3198811e1dfc16c89d47429bba93
David <brisingr.git@gmail.com>
2019-06-29 18:14:43 -0400

    Commit desde SRV

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_033537) do
+ActiveRecord::Schema.define(version: 2019_06_06_233211) do
 
   create_table "client_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 50
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 2019_05_24_033537) do
     t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "letter", limit: 5
   end
 
   create_table "tickets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
